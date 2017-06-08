231bdd643fb9f129783b3f875941f018433d675f
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2017-06-08 15:31:18 +0600

    new project uploaded

---
+++ CrystalReportDemo5/ReportForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CrystalReportDemo5
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.showReportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(1, 81);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ShowCloseButton = false;
+            this.crystalReportViewer1.ShowGroupTreeButton = false;
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.ShowParameterPanelButton = false;
+            this.crystalReportViewer1.ShowTextSearchButton = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1009, 474);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "End Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start Date";
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.endDateTimePicker.Location = new System.Drawing.Point(301, 21);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(111, 20);
+            this.endDateTimePicker.TabIndex = 3;
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.startDateTimePicker.Location = new System.Drawing.Point(87, 22);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(111, 20);
+            this.startDateTimePicker.TabIndex = 4;
+            // 
+            // showReportButton
+            // 
+            this.showReportButton.Location = new System.Drawing.Point(457, 9);
+            this.showReportButton.Name = "showReportButton";
+            this.showReportButton.Size = new System.Drawing.Size(124, 51);
+            this.showReportButton.TabIndex = 8;
+            this.showReportButton.Text = "Show Report";
+            this.showReportButton.UseVisualStyleBackColor = true;
+            this.showReportButton.Click += new System.EventHandler(this.showReportButton_Click);
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 554);
+            this.Controls.Add(this.showReportButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "ReportForm";
+            this.Text = "ReportForm";
+            this.Load += new System.EventHandler(this.ReportForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.Button showReportButton;
+    }
+}
---
+++ CrystalReportDemo5/ReportForm.cs
@@ -0,0 +1,66 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrystalReportDemo5
+{
+    public partial class ReportForm : Form
+    {
+      
+        public ReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportForm_Load(object sender, EventArgs e)
+        {
+            //DataTable dtReportData = GetData();
+            //ShowReport(dtReportData);
+        }
+
+        private void ShowReport(DataTable dtReportData)
+        {
+            ReportDocument rdoc = new ReportDocument();
+            rdoc.Load(@"Reports\CrystalReport1.rpt");
+            rdoc.SetDataSource(dtReportData);
+            crystalReportViewer1.ReportSource = rdoc;
+        }
+
+        private DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+
+           using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString))
+            {
+               using (SqlCommand cmd = new SqlCommand("getCustomersWithSalary", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@startDate", startDateTimePicker.Text);
+                    cmd.Parameters.AddWithValue("@endDate", endDateTimePicker.Text);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    dt.Load(reader);
+
+                }
+            }
+
+            return dt;
+        }
+
+        private void showReportButton_Click(object sender, EventArgs e)
+        {
+            DataTable dtReportData = GetData();
+            ShowReport(dtReportData);
+        }
+    }
+}
